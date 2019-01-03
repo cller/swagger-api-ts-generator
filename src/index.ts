10fd8cf2e2a27a905fc7f75e8f37785b1d55eba6
liuyang <liuyang@liegouchina.com>
2019-01-03 18:09:38 +0800

    修改npm包文件忽略文件，修改启动命令帮助提示

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+#! /usr/bin/env node
+
 import commander from 'commander';
 import { Downloader } from './downloader';
 import { Generator } from './generator';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
+const { version } = JSON.parse(readFileSync(join(__dirname, '../package.json'), { encoding: 'utf8' }));
 const downloader = new Downloader();
 const generator = new Generator();
 
@@ -9,12 +14,20 @@ downloader.api$.subscribe();
 generator.apiFile$.subscribe();
 
 commander
-  .version('0.0.1', '-v --version')
+  .version(version)
   .arguments('<uri>')
-  .option('-o, --output <dir>', 'Api output dir')
+  .option('-o, --output [dir], default', 'Api output dir, default: "./api"')
   .action((uri: string, options) => {
-    const { output } = options;
-    generator.generate(downloader.api$, output);
-    downloader.donwload(uri);
+    if (uri) {
+      const { output } = options;
+      generator.generate(downloader.api$, output || './api');
+      downloader.donwload(uri);
+    } else {
+      commander.outputHelp()
+    }
   })
   .parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  commander.outputHelp();
+}
